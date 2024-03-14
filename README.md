0ed1b0bd75f7ba3fb12b3fed5e6926921e592274
dwxcod62 <ducna1462@gmail.com>
2024-03-15 02:07:26 +0700

    upload solve

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Solve-C-Sharp
+
+<h3>There are ten files in the Data folder. Each file includes 499999 lines, each line includes ten strings and which issplit with ';', each string includes ten letters. Find out the 10 most frequent strings from all ten data files andshow them on the console<h3>
---
+++ Solution/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+using System;
+using System.Reflection;
+
+class Program
+{
+
+    static long StringToLong(string input)
+    {
+        long result = 0;
+        foreach (char c in input)
+        {
+            char lowerC = Char.ToLower(c);
+            result = result * 52 + (Char.IsLower(c) ? lowerC - 'a' : lowerC - 'a' + 26);
+        }
+        return result;
+    }
+
+    static string LongToString(long number)
+    {
+        string result = "";
+        const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        while (number > 0)
+        {
+            int index = (int)(number % 52);
+            char c = (index < 26) ? alphabet[index] : Char.ToUpper(alphabet[index - 26]);
+            result = c + result;
+            number /= 52;
+        }
+        return result.PadLeft(10, 'a');
+    }
+
+    static void Main()
+    {
+        string path = "..\\..\\..\\Data\\";
+        var stringCounts = new Dictionary<long, int>();
+        var resultCounts = new Dictionary<long, int>();
+
+        int index = 1;
+
+        for (int i = 0; i < 10 - 1; i++)
+        {
+            for (int j = i + 1; j < 10; j++)
+            {
+                stringCounts.Clear();
+                // Console.WriteLine(i + ":" + j);
+                Console.WriteLine(index + " / 45");
+                index++;
+
+                using (StreamReader reader = new StreamReader(path + "data" + i + ".dat"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] strings = line.Split(';');
+                        foreach (string str in strings)
+                        {
+                            long intValue = StringToLong(str);
+                            if (stringCounts.ContainsKey(intValue))
+                            {
+                                stringCounts[intValue]++;
+                            }
+                            else
+                            {
+                                stringCounts[intValue] = 1;
+                            }
+                        }
+                    }
+                }
+
+                using (StreamReader reader = new StreamReader(path + "data" + j + ".dat"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] strings = line.Split(';');
+                        foreach (string str in strings)
+                        {
+                            long intValue = StringToLong(str);
+                            if (stringCounts.ContainsKey(intValue))
+                            {
+                                stringCounts[intValue]++;
+                            }
+                            else
+                            {
+                                stringCounts[intValue] = 1;
+                            }
+                        }
+                    }
+                }
+
+                foreach (var kv in stringCounts)
+                {
+                    if (kv.Value > 1)
+                    {
+                        Console.WriteLine(LongToString(kv.Key));
+                        if (resultCounts.ContainsKey(kv.Key))
+                        {
+                            resultCounts[kv.Key]++;
+                        }
+                        else { resultCounts[kv.Key] = 1; }
+                    }
+                }
+
+            }
+        }
+
+
+        var top10 = resultCounts.OrderByDescending(kv => kv.Value).Take(10);
+
+        foreach (var kv in top10)
+        {
+            Console.WriteLine($"{LongToString(kv.Key)}: {kv.Value} lan");
+        }
+
+    }
+}
